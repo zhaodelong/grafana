c57d179777f7f3a18cfd80cdae883ae450b06b56
yangkb09 <yangkb09@gmail.com>
2022-02-25 18:08:41 -0500

    Chore: Create fake star service

--- pkg/api/http_server.go
+++ pkg/api/http_server.go
@@ -145,7 +145,7 @@ type HTTPServer struct {
 	AlertNotificationService     *alerting.AlertNotificationService
 	DashboardsnapshotsService    *dashboardsnapshots.Service
 	PluginSettings               *pluginSettings.Service
-	StarsService                 stars.Manager
+	StarManager                  stars.Manager
 }
 
 type ServerOptions struct {
@@ -249,7 +249,7 @@ func ProvideHTTPServer(opts ServerOptions, cfg *setting.Cfg, routeRegister routi
 		DashboardsnapshotsService:    dashboardsnapshotsService,
 		PluginSettings:               pluginSettings,
 		permissionServices:           permissionsServices,
-		StarsService:                 starsService,
+		StarManager:                  starsService,
 	}
 	if hs.Listener != nil {
 		hs.log.Debug("Using provided listener")
